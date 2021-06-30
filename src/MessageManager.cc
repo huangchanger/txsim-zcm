66509d9137cd6868b8073610b0e4224a3832404e
huangchanger <m18916085239@163.com>
2021-06-30 23:33:06 +0800

    MessageManager.cc

---
+++ src/MessageManager.cc
@@ -0,0 +1,101 @@
+#include "MessageManager.h"
+#include "my_module.h"
+#include "simutils.hpp"
+
+
+using std::list;
+using std::string;
+using std::vector;
+
+namespace txsim
+{
+    using namespace simutils;
+
+    MessageManager::MessageManager(const string &url)
+        : MessageManagerBase(url)
+    { }
+
+
+    void MessageManager::PackNavinfo(tx_sim::StepHelper& helper)
+    {
+        std::string loc_payload;
+        helper.GetSubscribedMessage(tx_sim::topic::kLocation, loc_payload);
+        sim_msg::Location loc;
+        loc.ParseFromString(loc_payload);
+
+        UTMCoor utmXY;
+        LatLonToUTMXY(loc.position().y(), loc.position().x(),utmXY);
+        std::lock_guard<std::mutex> lk(navinfo_mutex_);
+        navinfo_.timestamp          = helper.timestamp();
+        navinfo_.longitude          = loc.position().x();
+        navinfo_.latitude           = loc.position().y();
+        navinfo_.altitude           = loc.position().z();
+        navinfo_.utm_x              = utmXY.x;
+        navinfo_.utm_y              = utmXY.y;
+        navinfo_.angle_head         = loc.rpy().z();
+        navinfo_.angle_pitch        = loc.rpy().y();
+        navinfo_.angle_roll         = loc.rpy().x();
+        navinfo_.angular_vel_z      = loc.angular().z();
+        navinfo_.speed              = sqrt(pow(loc.velocity().x(),2)+pow(loc.velocity().y(),2)+pow(loc.velocity().z(),2));
+        navinfo_.velocity_east      = loc.velocity().x();
+        navinfo_.velocity_north     = loc.velocity().y();
+        navinfo_.curvature          = 0;
+        navinfo_.RTK_status         = 0;
+        navinfo_.HPOS_accuracy      = 0;
+        navinfo_.is_reckoning_vaild = 0;
+        navinfo_.gps_num_satellites = 0;
+        navinfo_.acceleration_x     = loc.acceleration().x(); //cheshen
+        navinfo_.acceleration_y     = loc.acceleration().y();
+        std::cout<<"packNavinfo succeed: "<<navinfo_.longitude<<" , "<<navinfo_.latitude<<std::endl;
+    }
+
+    void MessageManager::PackPredictedObject(tx_sim::StepHelper& helper)
+    {
+        std::string traffic_payload;
+        helper.GetSubscribedMessage(tx_sim::topic::kTraffic, traffic_payload);
+        sim_msg::Traffic traffic;
+        traffic.ParseFromString(traffic_payload);
+
+        std::lock_guard<std::mutex> lk(predictedObject_mutex_);
+        predictedObject_.time_stamp         = helper.timestamp();
+        predictedObject_.data_source        = 1;
+        predictedObject_.object_count       = traffic.staticobstacles_size() + traffic.dynamicobstacles_size();
+        for(int i=0; i<traffic.staticobstacles_size(); ++i){
+            PredictedObject obj;
+            auto static_obj = traffic.mutable_staticobstacles(i);
+            obj.id          = static_obj->id();
+            obj.type        = static_obj->type();
+            obj.velocity    = 0;
+            obj.accelerate  = 0;
+            obj.heading     = static_obj->heading();
+            obj.width       = static_obj->width();
+            obj.length      = static_obj->length();
+            
+        }
+        
+        
+
+
+
+    }
+
+    void MessageManager::SendTopicControl(tx_sim::StepHelper& helper)
+    {
+        sim_msg::Control cot;
+        std::string cot_payload;
+        cot.Clear();
+        cot.set_gear_cmd(sim_msg::Control::DRIVE);
+        cot.set_control_mode(sim_msg::Control::CM_AUTO_DRIVE);
+        cot.set_contrl_type(sim_msg::Control::ACC_CONTROL);
+        cot.mutable_acc_cmd()->set_acc(2);
+        cot.mutable_acc_cmd()->set_front_wheel_angle(0);
+
+        cot.SerializeToString(&cot_payload);
+        helper.PublishMessage(tx_sim::topic::kControl, cot_payload);
+        std::cout<<"kControl published\n";
+    }
+
+
+
+
+} // namespace tievsim
---
+++ src/MessageManagerBase.cc
@@ -0,0 +1,121 @@
+#include "MessageManagerBase.h"
+
+
+using std::string;
+using std::vector;
+
+namespace txsim
+{
+
+
+    // tunnel_(url) is deleted for a while to test Pack function
+    MessageManagerBase::MessageManagerBase(const string &url): need_stop_(false), chassisCommand_(), navinfo_()
+    {
+        kChannelNameChassisCommand = "ChassisCommand";
+
+        kChannelNameNavinfo = "Navinfo";
+        kChannelNamePredictedObject = "PredictedObject";
+
+        kFreqNavinfo = 20;
+        kFreqPredictedObject = 20;
+
+        kSwitchNavinfo = 1;
+        kSwitchPredictedObject = 1;
+     };
+
+    MessageManagerBase::~MessageManagerBase()
+    {
+        need_stop_ = true;
+        for (auto &t : pub_threads_)
+        {
+            if (t.joinable())
+                t.join();
+        }
+
+        tunnel_.stop();
+
+        for (auto t : sub_threads_)
+        {
+            tunnel_.unsubscribe(t);
+        }
+
+    }
+
+ // receive zcm
+    void MessageManagerBase::ChassisCommandHandler(
+        const zcm::ReceiveBuffer *rbuf,
+        const std::string &chan,
+        const MsgChassisCommandSignal *msg)
+    {
+        std::lock_guard<std::mutex> lock(chassisCommand_mutex_);
+        chassisCommand_.timestamp = msg->timestamp;
+        chassisCommand_.longitudinal_acceleration_command = msg->longitudinal_acceleration_command;
+        chassisCommand_.steer_wheel_angle_command = msg->steer_wheel_angle_command;
+        chassisCommand_.autonomous_mode_control_command = msg->autonomous_mode_control_command;
+        //printf("msg manager base aimsteer: %d, aimspeed: %d\n", cancontrol_.aimsteer, cancontrol_.aimspeed);
+    }
+
+
+    void MessageManagerBase::SubscribeAll()
+    {
+        std::cout<<"subscribe all "<<std::endl;
+        auto sub1 = tunnel_.subscribe(
+            kChannelNameChassisCommand, &MessageManagerBase::ChassisCommandHandler, this);
+
+        sub_threads_.push_back(sub1);
+
+        tunnel_.start();
+    }
+
+
+    // publishAllAsync -> PubLoopChannelname -> PublishChannelnameWithLock -> PublishChannelname
+    void MessageManagerBase::PublishNavinfo() const
+    {
+        std::cout<<"publish navinfo"<<std::endl;
+        tunnel_.publish(kChannelNameNavinfo, &navinfo_);
+        std::cout<<"navinfo zcm message published ! "<<std::endl;
+        std::cout<<navinfo_.longitude<<" , "<<navinfo_.latitude<<std::endl;
+    }
+
+
+    void MessageManagerBase::PublishNavinfoWithLock() const
+    {
+        std::lock_guard<std::mutex> caninfo_lock(navinfo_mutex_);
+        PublishNavinfo();
+    }
+
+    void MessageManagerBase::PubLoopNavinfo(int freq)
+    {
+        while (!need_stop_)
+        {
+            auto time_point = std::chrono::steady_clock::now() + std::chrono::microseconds(1000000 / freq);
+            PublishNavinfoWithLock();
+            std::this_thread::sleep_until(time_point);
+        }
+    }
+
+    // publish automatically with a certain frequency
+    void MessageManagerBase::PublishAllAsync()
+    {
+        if (kSwitchNavinfo)
+        {
+            pub_threads_.push_back(std::thread(
+                &MessageManagerBase::PubLoopNavinfo, this, kFreqNavinfo));
+        }
+
+        for (auto &t : pub_threads_)
+        {
+            t.detach();
+        }
+    }
+
+    // publish one time
+    void MessageManagerBase::PublishAll() const
+    {
+        if (kSwitchNavinfo)
+        {
+            PublishNavinfo();
+        }
+    }
+
+} // namespace tievsim
